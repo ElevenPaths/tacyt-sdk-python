b27588b08549419380f6c9387a222b0d6b0088d5
RubenMorenoLeyva <ruben.morenoleyva@gmail.com>
2015-09-28 11:08:26 +0200

    Auth and tacyt SDK
    Final version.

---
+++ tacyt/ExternalApiRequest.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+This library offers an API to use Tacyt in a python environment.
+Copyright (C) 2015 Eleven Paths
+'''
+
+class ExternalApiRequest(object):
+
+    JSON_FIELD_RESULT = "result"
+    JSON_FIELD_STATUS = "status"
+    JSON_FIELD_OK = "OK"
+    JSON_FIELD_ERROR = "ERROR"
+
---
+++ tacyt/Version.py
@@ -0,0 +1,7 @@
+class Version:
+
+    def __init__(self):
+        pass
+
+    API_VERSION = "1.3"
+    API_HOST = "path5.11paths.com"
