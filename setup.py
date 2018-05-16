5b5bd595b554f530423b370f569bbee387dfa9ad
Eva Suarez <eva.suarez22@gmail.com>
2018-05-16 12:41:13 +0200

    Add setup.py to allow installation via pip

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='tacytsdk',
+      version='1.7',
+      description='Tacyt SDK',
+      url='https://github.com/ElevenPaths/tacyt-sdk-python',
+      author='ElevenPaths',
+      author_email='',
+      license='GNU',
+      packages=find_packages())
