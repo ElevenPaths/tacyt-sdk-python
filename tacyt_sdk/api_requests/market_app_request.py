d5fa082127464e6f91356e957915dcf8dcd67311
javier.sanz <javier.sanzfrechilla@telefonica.com>
2020-03-26 10:19:41 +0100

    (Resolves #110650) Add new mandatory fields in API new market

--- tacyt_sdk/api_requests/market_app_request.py
+++ tacyt_sdk/api_requests/market_app_request.py
@@ -10,18 +10,21 @@ def _to_camel_case(field_name):
         return "appURL"
     if field_name == "direct_download_url":
         return "directDownloadURL"
+    if field_name == "find_date":
+        return "findDate"
     return to_camel_case(field_name)
 
 
 class NewMarketAppRequest(object):
     def __init__(self, unique_origin_id, unique_version_id, platform,
-                 origin, title, app_url, **extra_arguments):
+                 origin, title, app_url, find_date, **extra_arguments):
         self.unique_origin_id = unique_origin_id
         self.unique_version_id = unique_version_id
         self.platform = platform
         self.origin = origin
         self.title = title
         self.app_url = app_url
+        self.find_date = find_date
         self.extra_arguments = extra_arguments
 
     def as_dict(self):
@@ -31,7 +34,8 @@ class NewMarketAppRequest(object):
             "platform": self.platform,
             "origin": self.origin,
             "title": self.title,
-            "appURL": self.app_url
+            "appURL": self.app_url,
+            "findDate": self.find_date
         }
 
         for field_name in self.extra_arguments:
@@ -41,18 +45,26 @@ class NewMarketAppRequest(object):
 
 
 class UpdateMarketAppRequest:
-    def __init__(self, unique_origin_id, unique_version_id, origin,
-                 **extra_arguments):
+    def __init__(self, unique_origin_id, unique_version_id, platform,
+                 origin, title, app_url, find_date, **extra_arguments):
         self.unique_origin_id = unique_origin_id
         self.unique_version_id = unique_version_id
+        self.platform = platform
         self.origin = origin
+        self.title = title
+        self.app_url = app_url
+        self.find_date = find_date
         self.extra_arguments = extra_arguments
 
     def as_dict(self):
         data = {
             "uniqueOriginId": self.unique_origin_id,
             "uniqueVersionId": self.unique_version_id,
+            "platform": self.platform,
             "origin": self.origin,
+            "title": self.title,
+            "appURL": self.app_url,
+            "findDate": self.find_date
         }
         for field_name in self.extra_arguments:
             camel_case_name = _to_camel_case(field_name)
